57e39ebc43cdb807229d291aeec7492c9884ca57
garypenman <g.penman.1@research.gla.ac.uk>
2025-05-29 16:47:09 +0100

    Batching script to replaya ll of the mainz data nicely

---
+++ replay/SendRuns.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#dates=( "250203" "250204" "250205" "250206" )
+dates=( "250206" )
+#for dir in /w/work5/eic/Timepix/MainzTests/data.2502*;
+for date in ${dates[*]}
+do
+    dir=/w/work5/eic/Timepix/MainzTests/data.$date
+    #outfile=$date.txt
+    #echo $dir
+    #rm -f $outfile
+    #touch $outfile
+    for dir2 in $dir/run-*;
+    do
+	#echo $dir2
+	if [ -z "$(ls -A $dir2/ )" ];then
+	    continue;
+	fi
+	
+	files=($dir2/*)
+	file=${files[0]##"$dir2/"}
+	det=`echo $file | cut -d'-' -f 1`
+	dethalf=`echo $file | cut -d'-' -f 2`
+	date2=`echo $file | cut -d'-' -f 3`
+	time=`echo $file | cut -d'-' -f 4`
+	size=`du -kh $dir2 | cut -f1`
+	#echo $det, $time, $size >> $outfile
+	logfile="$det-$date-$time.log"
+	#nohup echo "test" 2>&1 >> $logfile &
+	nohup tpx4maketree -c -r $dir2/*.dat 2>&1 >> $logfile & 
+    done
+done
